3899a7590dcf94ea3eb3141e6901b837b759f94b
imearth <38610522+imearth@users.noreply.github.com>
2020-04-28 14:09:46 +0700

    Update Chart.vue

--- frontend/ar framework/src/components/Chart.vue
+++ frontend/ar framework/src/components/Chart.vue
@@ -280,7 +280,8 @@ export default {
 <style scope>
 #ChartCnvs {
   background-image: url(../assets/chartbg/targetbase.jpg);
-  background-size: 45% 50%; background-repeat:no-repeat;
+  background-size: 395px 400px; 
+  background-repeat:no-repeat;
   background-position: 55% 50%;
 }
 </style>
